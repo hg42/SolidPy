b26951858cd02ab1f1734b336843518d998991f8
hg42@gmx.net <hg42@gmx.net>
2014-12-29 15:57:12 +0100

    build.zsh (extruder)

--- build.zsh
+++ build.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
 #exec build test.solid.py
-atpath /home/harald/3D-print/rostock++/rostock-redesign2/carriage build carriage.solid.py
+#atpath /home/harald/3D-print/rostock++/rostock-redesign2/carriage build carriage.solid.py
+atpath /home/harald/3D-print/rostock++/rostock-redesign2/extruder build extruder.solid.py
